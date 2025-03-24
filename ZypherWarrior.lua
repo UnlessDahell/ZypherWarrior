27f7b377840eef78f76e9452e9b5c6c6350892db
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-24 08:02:43 +0700

    ZypherWarrior.lua

--- ZypherWarrior.lua
+++ ZypherWarrior.lua
@@ -3,17 +3,17 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Zypher Hub " V.0.1,
-    SubTitle = "by Zypher",
+    Title = "Fluent " .. Fluent.Version,
+    SubTitle = "by dawid",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Acrylic = true, 
     Theme = "Amethyst",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+    MinimizeKey = Enum.KeyCode.LeftControl 
 })
 
 local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "box" }),
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
